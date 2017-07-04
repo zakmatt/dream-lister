205abf723a72ae37cab67f32d604e8ca5186460c
matthew-zak <matthew.zak@tooploox.com>
2017-07-04 16:05:25 -0400

    add segue with printing details, modifying and/or deleting an object

--- DreamLister/ItemCell.swift
+++ DreamLister/ItemCell.swift
@@ -14,5 +14,11 @@ class ItemCell: UITableViewCell {
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var price: UILabel!
     @IBOutlet weak var details: UILabel!
-
+    
+    
+    func configureCell(item: Item) {
+        name.text = item.title
+        price.text = "$\(item.price)"
+        details.text = item.details
+    }
 }
--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -14,24 +14,48 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var segment: UISegmentedControl!
     
-    var controller: NSFetchedResultsController<Item>
+    var controller: NSFetchedResultsController<Item>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        generateTestData()
+        attemptFetch()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        configureCell(cell: cell, indexPath: indexPath as IndexPath)
+        return cell
+    }
+    
+    func configureCell(cell: ItemCell, indexPath: IndexPath) {
+        let item = controller.object(at: indexPath)
+        
+        cell.configureCell(item: item)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = controller.sections {
+            let sectionInfo = sections[section]
+            
+            return sectionInfo.numberOfObjects
+        }
+
         return 0
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        if let sections = controller.sections {
+            return sections.count
+        }
         return 0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
     
     func attemptFetch() {
@@ -40,6 +64,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         fetchRequest.sortDescriptors = [dateSort]
         
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        self.controller = controller
         
         do {
             try controller.performFetch()
@@ -73,7 +98,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         case.update:
             if let indexPath = indexPath {
                 let cell = tableView.cellForRow(at: indexPath) as! ItemCell
-                // update the cell data
+                configureCell(cell: cell, indexPath: indexPath)
             }
             break
         case.move:
@@ -85,7 +110,25 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
             }
             break
         }
+    }
+    
+    func generateTestData() {
+        let item = Item(context: context)
+        item.title = "MacBook Pro"
+        item.price = 1800
+        item.details = "I cannot wait till September event, I hope they release MBPs"
         
+        let item2 = Item(context: context)
+        item2.title = "Tesla S"
+        item2.price = 18000
+        item2.details = "It is beautiful! I need it"
+        
+        let item3 = Item(context: context)
+        item3.title = "Boose"
+        item3.price = 1800
+        item3.details = "Finally, noise-cancelling headphones!"
+        
+        ad.saveContext()
     }
 }
 
